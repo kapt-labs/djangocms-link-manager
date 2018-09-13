543a818d24d4c2b49729519f08dd01233f07c89f
Adrien Delhorme <adrien.delhorme@gmail.com>
2018-09-13 10:35:21 +0200

    FIX: check_link validate emails and phone numbers

--- djangocms_link_manager/link_managers/cmsplugin_link.py
+++ djangocms_link_manager/link_managers/cmsplugin_link.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-
+from django.utils.translation import ugettext as _
 from django.core.urlresolvers import NoReverseMatch
 
 from ..link_manager import LinkManager, LinkReport
@@ -10,19 +10,30 @@ from ..link_manager import LinkManager, LinkReport
 class CMSPluginLinkLinkManager(LinkManager):
 
     def check_link(self, instance, verify_exists=False):
-        internal_link = instance.internal_link
         valid = False
 
-        if internal_link:
+        if instance.internal_link is not None:
             try:
-                url = internal_link.get_absolute_url(instance.language)
+                url = instance.internal_link.get_absolute_url(instance.language)
             except NoReverseMatch:
                 url = None
             else:
                 valid = True
-        else:
+
+        elif instance.external_link is not '':
             url = instance.external_link
             valid = self.validate_url(url, verify_exists=verify_exists)
+
+        elif instance.mailto is not '':
+            url = instance.mailto
+            valid = self.validate_mailto(url, verify_exists=verify_exists)
+
+        elif instance.phone is not '':
+            url = instance.phone
+            valid = self.validate_tel(url, verify_exists=verify_exists)
+
+        else:
+            url = _('No link')
 
         return LinkReport(
             valid=valid,
